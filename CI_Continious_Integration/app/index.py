d7bffa7ec82d9af658563c54ced467c6c2c409b7
silent-mobius <alex@vaiolabs.com>
2019-07-31 17:01:54 +0300

    adding python details

--- CI_Continious_Integration/app/index.py
+++ CI_Continious_Integration/app/index.py
@@ -5,6 +5,6 @@
 print("Content-type: text/html\n\n")
 print("<html>\n<body>")
 print("<div style=\"width: 100%; font-size: 40px; font-weight: bold; text-align: center;\">")
-print("Python Script Test Page")
+print("WOW!!!! Cool Python APP appeared!!!")
 print("</div>\n</body>\n</html>")
 
