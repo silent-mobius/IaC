8e3dfa86d69970433b28fa6b37f2cd2d08e0247f
silent-mobius <alex@vaiolabs.com>
2019-07-31 16:58:54 +0300

    adding demo app

---
+++ CI_Continious_Integration/app/index.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+#demo app
+
+
+print("Content-type: text/html\n\n")
+print("<html>\n<body>")
+print("<div style=\"width: 100%; font-size: 40px; font-weight: bold; text-align: center;\">")
+print("Python Script Test Page")
+print("</div>\n</body>\n</html>")
+
