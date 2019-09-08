7b99f27314669230b4fcb53dcf47d65e4b98f388
pushtakio <alex@vaiolabs.com>
2019-09-08 17:16:54 +0300

    created hello bash type script

--- rpm_build/sources/bello.sh
+++ rpm_build/sources/bello.sh
@@ -4,5 +4,50 @@
 #developer: pushtakio
 #purpose: to say hello
 #date: 08/09/2019
-#ver: 1.0.0
+#ver: 1.0.12
 ################################################################################
+
+
+####Variables::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+_time=2
+line="#########################################################################"
+msg_root="This Tool Can Not Be Used With $EUID User"
+msg_permssions="Please Decrease your level toregular user"
+####Functions /\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
+deco(){
+  pre="$line"
+  post="$line"
+  printf "\n$pre\n#%s\n$post\n" "$@"
+
+}
+
+hello(){
+ user=$1
+ sleep 1
+ if [[ "$@" == "" ]];then
+   deco "Hello $USER"
+else
+  deco "Hello $@"
+fi
+}
+
+
+####
+#Main _ -_ -_ -_ -_ -_ -_ -_ -_ -_ -_ -_ -_ -_ -_ -_ -_ -_ -_ -_ -_ -_ -_ -_ -
+####
+
+if [[ $EUID == "0" ]];then
+  clear
+  deco "$msg_root"
+  sleep $_time
+  clear
+  deco "$msg_permssions"
+  sleep $_time
+  clear
+  exit 1
+else
+  clear
+    hello "$@"
+      sleep $_time
+  clear
+fi
